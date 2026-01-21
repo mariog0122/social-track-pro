c748e5d8a12f724a1894ebecb02e9a14d3ef5316
mariog0122 <mariogomezcorrea8@gmail.com>
2026-01-20 22:42:18 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  resolve: {
-    alias: {
-      '@': '/'
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: false
+    },
+    resolve: {
+      alias: {
+        '@': './'
+      }
     }
-  }
+  };
 });
